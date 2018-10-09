6290aa71ba24cbe4d00d8e8e33c57fbc005cf906
Chris Pak <chris.pak125@gmail.com>
2018-10-09 11:32:17 -0400

    Create new user route works

--- routes/usersController.js
+++ routes/usersController.js
@@ -1,13 +1,21 @@
 const router = require('express').Router()
 const { User } = require('../db/model')
 
+//SHOW ALL
 router.get('/', async (req, res) => {
     const users = await User.find()
     res.send(users)
 })
 
+//SHOW ONE
 router.get('/:id', async (req,res)=>{
     const user = await User.findById(req.params.id)
+    res.send(user)
+})
+
+//CREATE
+router.post('/', async (req,res)=>{
+    const user = await User.create(req.body)
     res.send(user)
 })
 
