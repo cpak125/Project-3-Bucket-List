359cf163b7f0250bf9fb24ef46a3a48219027e20
Chris Pak <chris.pak125@gmail.com>
2018-10-09 09:58:53 -0400

    Added seeds data

---
+++ bin/db/seeds.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const mongoose = require('mongose')
+
+mongoose.connect(
+    process.env.MONGODB_URI,
+    {useNewUrlParser: true}
+)
+
+const {User, Item}= require('./model')
+
+const skydive = new Item({
+    completed: false,
+    description: 'Go skydiving'
+})
+
+const family = new Item({
+    completed: false,
+    description: 'Have a family with mulitple kids'
+})
+
+const europe = new Item({
+    completed: false,
+    description: 'Travel to Europe'
+})
+
+const tom = new User({
+    name: 'Tom',
+    bucketList:[skydive, family, europe]
+})
+
+User.remove({})
+  .then(() => elon.save())
+  .then(() => console.log('Successful Save'))
+  .then(() => mongoose.connection.close())
