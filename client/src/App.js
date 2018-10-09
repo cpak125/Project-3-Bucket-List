39edbb1199af383bc8e71cb7fa36d4a53e472e65
Chris Pak <chris.pak125@gmail.com>
2018-10-09 10:27:47 -0400

    Home route works

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div >
-        Hello React
-      </div>
+      <Router >
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
