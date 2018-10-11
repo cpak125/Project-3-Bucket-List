95e9ec64546387b3c754326f9e2f1ef8a707927b
Chris Pak <chris.pak125@gmail.com>
2018-10-10 21:42:01 -0400

    Added create and delete to items controller

--- routes/itemsController.js
+++ routes/itemsController.js
@@ -8,4 +8,32 @@ router.get('/', async (req, res) => {
     res.send(items)
   })
 
+  //CREATE
+  router.post('/', (req, res) => {
+    const newIdea = new Idea()
+
+    User.findById(req.params.userId)
+        .then((user) => {
+            user.ideas.push(newIdea)
+            return user.save()
+        })
+        .then((user) => {
+            res.send(user)
+        })
+})
+
+//DELETE
+router.delete('/:id', (req, res) => {
+    User.findById(req.params.userId)
+        .then(user => {
+            //HELP
+            return user.update({ $pull: { ideas: { _id: req.params.id } } })
+        })
+        .then(user => {
+            res.send(user)
+        })
+})
+
+
+
   module.exports = router
